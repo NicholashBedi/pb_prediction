33bb7f81dc0f854bed67732b324494e5a2eeb223
Nicholash Bedi <nbedi@uwaterloo.ca>
2020-01-11 18:54:54 -0500

    improved data formation and documentation

--- format_data_for_ML.py
+++ format_data_for_ML.py
@@ -1,20 +1,7 @@
+# See get data function
 import numpy as np
 from athletes_PBS import convert_to_seconds_from_string
 
-data_names = {'name':0,
-                'is_women':1,
-                'PB':2,
-                'place':3,
-                '300m':4,
-                '700m':5,
-                '1100m':6,
-                '1500m':7}
-input_names = {'is_women':0,
-                'place':1,
-                '300m':2,
-                '700m':3,
-                '1100m':4,
-                '1500m':5}
 def remove_unused_data(data):
     # Remove unused data
     # 0:Name,1:Gender,2:PB,3:SB,4:Race,5:Place,
@@ -30,6 +17,14 @@ def remove_unused_data(data):
 
 def break_into_name_input_target(data):
     o_data = {}
+    data_names = {'name':0,
+                    'is_women':1,
+                    'PB':2,
+                    'place':3,
+                    '300m':4,
+                    '700m':5,
+                    '1100m':6,
+                    '1500m':7}
     o_data["input"] = np.vstack((data[:,data_names['is_women']],
                             data[:,data_names['place']],
                             data[:,data_names['300m']],
@@ -40,9 +35,19 @@ def break_into_name_input_target(data):
     o_data["name"] = data[:,data_names['name']]
     return o_data
 
+# Changes data so that instead of m and w it is 1 and 0
+# Changes times from string to float
+# Changes target to how much slower this race was compared to PB (ie. How much faster athlete can go)
+# Changes 300m, 700m and 1100m time to how far off pace they were at a given point
 def reformat_data(data):
     # Change gender to is_women
     replace_gender_string = {'w': 1, 'm': 0}
+    input_names = {'is_women':0,
+                    'place':1,
+                    '300m':2,
+                    '700m':3,
+                    '1100m':4,
+                    '1500m':5}
     data["input"][:, input_names['is_women']] = [replace_gender_string[gender] for gender in data["input"][:, input_names['is_women']]]
 
     # change times from string to number
@@ -51,13 +56,15 @@ def reformat_data(data):
                 input_names['1100m'], input_names['1500m']]:
         data["input"][:,idx] = [convert_to_seconds_from_string(string_time) for string_time in data["input"][:,idx]]
     data["input"] = data["input"].astype(float)
+    data["target"] = np.minimum(data["target"], data["input"][:,input_names['1500m']])
+    # Change target to how much faster PB is compared to actual time
     data["target"] = data["input"][:,input_names['1500m']] - data["target"]
     # Change data to pace Difference
     distances_to_convert = [300, 700, 1100]
     for d in distances_to_convert:
         data["input"][:, input_names[str(d)+'m']] = \
-                                  data["input"][:,input_names[str(d)+'m']]\
-                                - (data["input"][:,input_names['1500m']]*(d/1500))
+                                (data["input"][:,input_names['1500m']]*(d/1500)) \
+                                - data["input"][:,input_names[str(d)+'m']]
     return data
 
 
@@ -70,11 +77,10 @@ def break_data_into_components(data, training_percent = 0.8, testing_percent = 0
     else:
         validation_percent = 1 - training_percent - testing_percent
     num_data = np.shape(data["name"])[0]
-    data_type = ["input", "target", "name"]
     o_data["training"] = {}
     o_data["validation"] = {}
     o_data["testing"] = {}
-    for type in data_type:
+    for type in ["input", "target", "name"]:
         o_data["training"][type] = data[type][:round(num_data*training_percent)]
         o_data["validation"][type] = data[type][round(num_data*training_percent):
                              round(num_data*(training_percent+validation_percent))]
@@ -97,13 +103,13 @@ def whiten_data(data):
         data["validation"]["input"][:,i] -= data["mean_input"][i]
         data["testing"]["input"][:,i] -= data["mean_input"][i]
 
-    data["norm_imput"] = np.linalg.norm(data["training"]["input"], axis=0)
-    data["norm_imput"][data["norm_imput"] ==0] = 1
-    data["norm_imput"][0] = 1
+    data["norm_input"] = np.linalg.norm(data["training"]["input"], axis=0)
+    data["norm_input"][data["norm_input"] ==0] = 1
+    data["norm_input"][0] = 1
     for i in range(1,num_input): # Can skip 0 because it should be norm to 1
-        data["training"]["input"][:,i] /= data["norm_imput"][i]
-        data["validation"]["input"][:,i] /= data["norm_imput"][i]
-        data["testing"]["input"][:,i] /= data["norm_imput"][i]
+        data["training"]["input"][:,i] /= data["norm_input"][i]
+        data["validation"]["input"][:,i] /= data["norm_input"][i]
+        data["testing"]["input"][:,i] /= data["norm_input"][i]
 
     data["mean_target"] = np.mean(data["training"]["target"])
     data["training"]["target"] -= data["mean_target"]
@@ -116,37 +122,83 @@ def whiten_data(data):
     data["testing"]["target"] /= data["norm_target"]
     return data
 
+# Returns data in the following format
+# data["name"]:
+#       Returns the athlete name and race they ran
+# Next the data is broken into three sections:
+# training, validation and testing
+# eg. data["training"]
+# In each of these section the data is formated as follows
+# Target:
+#       How much faster the athlete can go. (the difference between this races time and their PB)
+#       (positive float)
+# Input:
+#       is_women: 0 if man, 1 if women
+#       place: What place they finished in the race (positive int)
+#       How much faster they were at 300m compared to an even split race (Float)
+#       How much faster they were at 700m compared to an even split race (Float)
+#       How much faster they were at 1100m compared to an even split race (Float)
+#       Their 1500m finishing time
+# Note, the data may be whitened
 def get_data(data_folder = "", data_name = "test_data",
-            training_percent = 0.8, testing_percent = 0.1):
+            training_percent = 0.8, testing_percent = 0.1,
+            whiten = True, seed = 0):
     data = np.genfromtxt(data_folder + data_name + ".txt",
                                   dtype=np.unicode_, delimiter=',', skip_header=1)
     data = discard_unknown_data(data)
     data = remove_unused_data(data)
     # New indexes - Can now use data_names dictionary
     # 0:Name,1:Gender,2:PB,3:Place,4:300m,5:700m,6:1100m,7:1500m
-    np.random.seed(0)
+    np.random.seed(seed)
     np.random.shuffle(data)
     data = break_into_name_input_target(data)
     data = reformat_data(data)
 
     data = break_data_into_components(data, training_percent, testing_percent)
-    data = whiten_data(data)
+    if (whiten):
+        return whiten_data(data)
     return data
 
-def print_data(data):
+def print_data(data, keep_whiten = False, norm_input = [], mean_input = [],
+                norm_target = 1, mean_target = 0, predictions = []):
     num_data = np.shape(data["name"])[0]
+    is_prediction = (len(predictions) == num_data)
+    print("{0:>40}|  {1:>6}; {2:>6}; {3:>6}; {4:>6}; {5:>6}; {6:>6};| {7:>7}".format(
+            "name", "women?", "place", "300m", "700m", "1100m", "final", "PB diff"), end="")
+    if is_prediction:
+        print("| {0:>7}| {1:>7}".format("Predict", "Abs Error"))
+        print("{0:->115}".format(""))
+    else:
+        print("\n{0:->99}".format(""))
+    total_abs_error = 0
+    total_square_error = 0
     for i in range(0, num_data):
-        print(data["name"][i], end="; ")
+        print("{0:<40}".format(data["name"][i]), end="| ")
         for j in range(0,6):
-            print("{0:4.2f}".format(data["input"][i,j]), end="; ")
-        print("\t", end="")
-        print("{0:4.2f}".format(data["target"][i]))
+            if (keep_whiten):
+                print(" {0:6.2f}".format(data["input"][i,j]), end=";")
+            else:
+                print("{0:6.2f}".format(data["input"][i,j] \
+                                        * norm_input[j] \
+                                        + mean_input[j]), end="; ")
+        real_target = data["target"][i]*norm_target + mean_target
+        print("| {0:7.2f}".format(real_target), end="")
+        if (is_prediction):
+            abs_error = abs(predictions[i] - real_target)
+            total_square_error += (predictions[i] - real_target)**2
+            total_abs_error += abs_error
+            print("| {0:7.2f}| {1:6.2f}".format(predictions[i],abs_error))
+    if num_data == 0:
+        return 0, 0
+    return total_abs_error/num_data, total_square_error/num_data
 
 if __name__ == '__main__':
     data_folder = ""
     data_name = "test_data"
+    data_name = "combined_results_2019"
     data = get_data(data_folder = data_folder, data_name = data_name,
-                training_percent = 0.8, testing_percent = 0.1)
+                training_percent = 0.8, testing_percent = 0.1,
+                whiten = False)
 
     print("\nTraining")
     print_data(data["training"])
--- generating_model.py
+++ generating_model.py
@@ -4,38 +4,6 @@ import numpy as np
 import format_data_for_ML
 import athletes_PBS
 
-model_name = "initial"
-data_folder = ""
-data_name = "combined_results_2019"
-
-training_percent = 0.6
-testing_percent = 0.2
-# validation_percent = 1 - training_percent - testing_percent
-
-data = format_data_for_ML.get_data(data_folder = data_folder, data_name = data_name,
-            training_percent = training_percent, testing_percent = testing_percent)
-
-
-reg_lamb = 0.00003
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(12, activation="relu", input_shape=(6,),
-                        kernel_regularizer=tf.keras.regularizers.l2(reg_lamb),
-                        kernel_initializer=tf.keras.initializers.RandomNormal(
-                                            mean=0.0, stddev=0.5, seed=None)),
-    tf.keras.layers.Dense(12, activation="relu",
-                        kernel_regularizer=tf.keras.regularizers.l2(reg_lamb),
-                        kernel_initializer=tf.keras.initializers.RandomNormal(
-                                            mean=0.0, stddev=0.5, seed=None)),
-    tf.keras.layers.Dense(12, activation="relu",
-                          kernel_regularizer=tf.keras.regularizers.l2(reg_lamb),
-                          kernel_initializer=tf.keras.initializers.RandomNormal(
-                                              mean=0.0, stddev=0.5, seed=None)),
-    tf.keras.layers.Dense(1)
-])
-
-optim = tf.keras.optimizers.Adam(lr=0.00001)
-
 def get_huber_loss_fn(**huber_loss_kwargs):
 
     def custom_huber_loss(y_true, y_pred):
@@ -43,72 +11,101 @@ def get_huber_loss_fn(**huber_loss_kwargs):
 
     return custom_huber_loss
 
-model.compile(optimizer = optim,
-              loss =get_huber_loss_fn(delta=0.9),
-              metrics =['mean_absolute_error'])
+def main():
+    model_name = "initial"
+    data_folder = ""
+    data_name = "combined_results_2019"
 
-es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min',
-                    verbose=0, patience=1000)
-mc = tf.keras.callbacks.ModelCheckpoint('best_model.h5',
-                    monitor='val_loss',
-                    mode='min', verbose=0,
-                    save_best_only=True)
+    training_percent = 0.6
+    testing_percent = 0.2
+    # validation_percent = 1 - training_percent - testing_percent
 
-history = model.fit(x=data["training"]["input"],
-        y=data["training"]["target"],
-        epochs=10001,
-        batch_size=32,
-        verbose=1,
-        validation_data=(data["validation"]["input"], data["validation"]["target"]),
-        callbacks=[es, mc])
-saved_model = tf.keras.models.load_model('best_model.h5',custom_objects={'custom_huber_loss': get_huber_loss_fn(delta=0.1)})
-if (testing_percent > 0):
-    test_loss, test_acc = saved_model.evaluate(x=data["testing"]["input"],
-                                        y=data["testing"]["target"],
-                                        batch_size=32)
-    print('Test accuracy:', test_acc)
-    print('Test loss:', test_loss)
-    predictions = saved_model.predict(x=data["testing"]["input"]).T *data["norm_target"] + data["mean_target"]
-    print(predictions)
-    real_target = data["testing"]["target"]*data["norm_target"] + data["mean_target"]
-    for i in range(0, len(data["testing"]["target"])):
-        print(data["testing"]["name"][i], end = "\t; ")
-        for j in range(0,6):
-            print("{0:6.2f}".format(data["testing"]["input"][i,j] \
-                                    * data["norm_imput"][j] \
-                                    + data["mean_input"][j]), end="\t; ")
-        print(end="\t")
-        print("{0:7.3f} \t {1:7.3f}".format(real_target[i], predictions[0][i]))
-
-# Plot training & validation accuracy svalues
-plt.plot(history.history['mean_absolute_error'])
-plt.plot(history.history['val_mean_absolute_error'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
+    data = format_data_for_ML.get_data(data_folder = data_folder, data_name = data_name,
+                training_percent = training_percent, testing_percent = testing_percent)
 
 
-# if (input('Press s to save model or anykey otherwise:') == "s"):
-#     print("Saving model")
-#     SAVE_MODEL = True
-# else:
-#     print("Discarding model")
-#     SAVE_MODEL = False
-# # serialize model to JSON
-# if (SAVE_MODEL):
-#     model_json = saved_model.to_json()
-#     with open(base_folder + model_name + ".json", "w") as json_file:
-#         json_file.write(model_json)
-#     # serialize weights to HDF5
-#     saved_model.save_weights(base_folder + model_name + ".h5")
-#     print("Saved model to disk")
+    run_model = False
+    if run_model:
+        reg_lamb = 0.00003
+
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(12, activation="relu", input_shape=(6,),
+                                kernel_regularizer=tf.keras.regularizers.l2(reg_lamb),
+                                kernel_initializer=tf.keras.initializers.RandomNormal(
+                                                    mean=0.0, stddev=0.5, seed=None)),
+            tf.keras.layers.Dense(12, activation="relu",
+                                kernel_regularizer=tf.keras.regularizers.l2(reg_lamb),
+                                kernel_initializer=tf.keras.initializers.RandomNormal(
+                                                    mean=0.0, stddev=0.5, seed=None)),
+            tf.keras.layers.Dense(12, activation="relu",
+                                  kernel_regularizer=tf.keras.regularizers.l2(reg_lamb),
+                                  kernel_initializer=tf.keras.initializers.RandomNormal(
+                                                      mean=0.0, stddev=0.5, seed=None)),
+            tf.keras.layers.Dense(1)
+        ])
+
+        optim = tf.keras.optimizers.Adam(lr=0.00001)
+
+        model.compile(optimizer = optim,
+                      loss = get_huber_loss_fn(delta=0.9),
+                      metrics =['mean_absolute_error'])
+
+        es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min',
+                            verbose=0, patience=1000)
+        mc = tf.keras.callbacks.ModelCheckpoint('best_model.h5',
+                            monitor='val_loss',
+                            mode='min', verbose=0,
+                            save_best_only=True)
+
+        history = model.fit(x=data["training"]["input"],
+                y=data["training"]["target"],
+                epochs=10001,
+                batch_size=32,
+                verbose=1,
+                validation_data=(data["validation"]["input"], data["validation"]["target"]),
+                callbacks=[es, mc])
+
+    saved_model = tf.keras.models.load_model('best_model.h5',custom_objects={'custom_huber_loss': get_huber_loss_fn(delta=0.1)})
+    if (testing_percent > 0):
+        test_loss, test_acc = saved_model.evaluate(x=data["testing"]["input"],
+                                            y=data["testing"]["target"],
+                                            batch_size=32)
+        print('Test accuracy:', test_acc)
+        print('Test loss:', test_loss)
+        predictions = saved_model.predict(x=data["testing"]["input"]).T *data["norm_target"] + data["mean_target"]
+        print(predictions)
+        real_target = data["testing"]["target"]*data["norm_target"] + data["mean_target"]
+        for i in range(0, len(data["testing"]["target"])):
+            print(data["testing"]["name"][i], end = "\t; ")
+            for j in range(0,6):
+                print("{0:6.2f}".format(data["testing"]["input"][i,j] \
+                                        * data["norm_input"][j] \
+                                        + data["mean_input"][j]), end="\t; ")
+            print(end="\t")
+            print("{0:7.3f} \t {1:7.3f}".format(real_target[i], predictions[0][i]))
+
+    # Plot training & validation accuracy svalues
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    if (input('Press s to save model or anykey otherwise:') == "s"):
+        print("Saving model")
+        SAVE_MODEL = True
+    else:
+        print("Discarding model")
+        SAVE_MODEL = False
+    # serialize model to JSON
+    if (SAVE_MODEL):
+        model_json = saved_model.to_json()
+        with open(base_folder + model_name + ".json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        saved_model.save_weights(base_folder + model_name + ".h5")
+        print("Saved model to disk")
+if __name__ == '__main__':
+    main()
